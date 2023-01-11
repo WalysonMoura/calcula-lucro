a964370d605cf68468e020e50fdd9a48df3e3a6f
Walyson Moura <walysonmoura222@gmail.com>
2023-01-11 15:50:59 -0300

    new

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,9 @@
 
-import { Inter } from "@next/font/google";
-
 import * as Style from "../styles/pages/home";
 
 import Promotion from "../components/Promotion";
 import Pricing from "../components/Pricing";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
